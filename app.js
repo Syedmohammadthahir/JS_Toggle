a26d1872da22e956f4994c9d5bdadda238399c8e
Syed Thahir <34040173+Syedmohammadthahir@users.noreply.github.com>
2019-01-09 12:22:26 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+
+
+
+
+const input = document.querySelector('input.description');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button.description');
+
+
+
+toggleList.addEventListener('click', () => {
+
+ // toggleList.textContent = 'Show list'; 
+ // listDiv.style.display = 'none'                       
+ //  //   listDiv.style.display = 'none';
+
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Show list';                        
+    listDiv.style.display = 'none';
+  }                         
+});
+
+
+
+
+button.addEventListener('click', () => {
+  p.innerHTML = input.value + ':';
+});
